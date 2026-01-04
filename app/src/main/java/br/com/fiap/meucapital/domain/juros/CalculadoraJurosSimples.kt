78f2da2445cc6104b01de5a84397bc1eec3f63f3
Prof. Celso <celso@celso.dev.br>
2026-01-04 09:24:39 -0300

    Ajustes

--- app/src/main/java/br/com/fiap/meucapital/domain/juros/CalculadoraJurosSimples.kt
+++ app/src/main/java/br/com/fiap/meucapital/domain/juros/CalculadoraJurosSimples.kt
@@ -2,17 +2,23 @@ package br.com.fiap.meucapital.domain.juros
 
 class CalculadoraJurosSimples {
 
+    // Função para calcular juros simples
     fun calcularJurosSimples(capital: Double, taxa: Double, tempo: Int): Double {
 
         if (capital < 0 || taxa < 0 || tempo < 0) {
-            throw IllegalArgumentException("Os valores de capital, taxa e tempo devem ser positivos.")
+            throw IllegalArgumentException(
+                "Os valores de capital, taxa e tempo devem ser positivos."
+            )
         }
         return capital * (taxa / 100) * tempo
     }
 
+    // Função para calcular montante
     fun calcularMontante(capital: Double, taxa: Double, tempo: Int): Double {
         if (capital < 0 || taxa < 0 || tempo < 0) {
-            throw IllegalArgumentException("Os valores de capital, taxa e tempo devem ser positivos.")
+            throw IllegalArgumentException(
+                "Os valores de capital, taxa e tempo devem ser positivos."
+            )
         }
         return capital + calcularJurosSimples(capital, taxa, tempo)
     }
